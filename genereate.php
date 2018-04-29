24a1a81966883fd8201b5c9b9b8efb811e26f738
Thorbjörn Backlund <thorbjorn@spiro.se>
2018-04-29 10:53:57 +0200

    Support non github packages

--- genereate.php
+++ genereate.php
@@ -18,14 +18,27 @@
 		}
 	}
 
+	foreach(explode(PHP_EOL, file_get_contents(__DIR__ . '/external.list')) as $external_row)
+	{
+		$external_row = trim($external_row);
+		if($external_row)
+		{
+			list($name_path, $full_path) = explode(' ', $external_row, 2) + [NULL, NULL];
+			if($name_path AND $full_path)
+			{
+				add_external($name_path, $full_path);
+			}
+		}
+	}
+
 	file_put_contents(__DIR__ . '/packages.json', json_encode(['packages' => $packages], JSON_PRETTY_PRINT));
 
-	function add_github($github_path, $type = NULL)
+	function add_github($repo_name_path, $type = NULL)
 	{
 		global $remote_site;
 		global $packages;
 
-		$raw = $remote_site->get_page("https://api.github.com/repos/{$github_path}/git/refs/tags");
+		$raw = $remote_site->get_page("https://api.github.com/repos/{$repo_name_path}/git/refs/tags");
 
 		/** @var phpdoc_github_tag[] $json */
 		$json = json_decode($raw);
@@ -39,46 +52,79 @@
 				{
 					continue;
 				}
-				if(substr($tag, -6) === '-baked')
-				{
-					$tag = substr($tag, 0, -6) . '-p1';
-				}
 				if(!preg_match(VERSION_REGEXP, $tag))
 				{
 					continue;
 				}
-				if(isset($packages[$github_path][$tag]))
+				if(isset($packages[$repo_name_path][$tag]))
 				{
 					continue;
 				}
 
 				$package_json = NULL;
-				$package_json_raw = $remote_site->get_page("https://raw.githubusercontent.com/{$github_path}/{$tag_data->object->sha}/composer.json");
+				$package_json_raw = $remote_site->get_page("https://raw.githubusercontent.com/{$repo_name_path}/{$tag_data->object->sha}/composer.json");
 
 				if($package_json_raw)
 				{
 					$package_json = json_decode($package_json_raw, TRUE);
 				}
 
-				if($package_json)
+				if(!$package_json)
 				{
-					$packages[$github_path][$tag] = $package_json;
+					$package_json = ['name' => $repo_name_path];
 				}
-				else
+				$package_json['version'] = $tag;
+				if($type AND empty($package_json['type']))
 				{
-					$packages[$github_path][$tag]['name'] = $github_path;
+					$package_json['type'] = $type;
 				}
-				$packages[$github_path][$tag]['version'] = $tag;
-				if($type AND empty($packages[$github_path][$tag]['type']))
-				{
-					$packages[$github_path][$tag]['type'] = $type;
-				}
-				$packages[$github_path][$tag]['source'] = [
+				$package_json['source'] = [
 					'reference' => $tag_data->object->sha,
 					'type' => 'git',
-					'url' => 'https://github.com/' . $github_path . '.git',
+					'url' => 'https://github.com/' . $repo_name_path . '.git',
 				];
+				$packages[$repo_name_path][$tag] = $package_json;
 			}
+		}
+	}
+
+	function add_external($repo_name_path, $full_path)
+	{
+		global $packages;
+
+		$save_path = escapeshellarg($full_path);
+		$cmd = 'git ls-remote --tags ' . $save_path;
+		foreach(explode(PHP_EOL, shell_exec($cmd)) as $row)
+		{
+			$columns = explode("\t", $row);
+			if(count($columns) !== 2)
+			{
+				continue;
+			}
+
+			list($hash, $ref) = $columns;
+			$tag = substr($ref, strlen(TAG_REF_PREFIX));
+			if($ref !== TAG_REF_PREFIX . $tag)
+			{
+				continue;
+			}
+
+			$safe_tag = escapeshellarg($tag);
+			$cmd = 'git archive --remote=' . $save_path . ' ' . $safe_tag . ' composer.json | tar xf - --to-stdout';
+			$json_raw = shell_exec($cmd);
+			$package_json = json_decode($json_raw, TRUE);
+
+			if(!$package_json)
+			{
+				$package_json = ['name' => $repo_name_path];
+			}
+			$package_json['version'] = $tag;
+			$package_json['source'] = [
+				'reference' => $hash,
+				'type' => 'git',
+				'url' => $full_path,
+			];
+			$packages[$repo_name_path][$tag] = $package_json;
 		}
 	}
 
@@ -101,14 +147,3 @@
 	class phpdoc_github_tag
 	{
 	}
-
-	/*
-  {
-    "ref": "refs/tags/2.0.7",
-    "url": "https://api.github.com/repos/SpiroAB/WordPress/git/refs/tags/2.0.7",
-    "object": {
-      "sha": "7b1b18a0bfd7ade1f770b09e7d1e2bdbc693ec3b",
-      "type": "commit",
-      "url": "https://api.github.com/repos/SpiroAB/WordPress/git/commits/7b1b18a0bfd7ade1f770b09e7d1e2bdbc693ec3b"
-    }
-  },	 * */
